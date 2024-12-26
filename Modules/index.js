ddcb54a0c8a1945939d42958843fb60a3d60c22f
ChiragMehra1212 <chiragmehra03.hp@gmail.com>
2024-12-26 16:11:15 +0530

    update

---
+++ Modules/index.js
@@ -0,0 +1,12 @@
+// import sname from "./operations.js";
+// console.log(sname);
+// import skills from "./operations.js";
+// console.log(skills);
+
+import { additon,subtraction,multiplication,division} from "./operations.js";
+// console.log(additon);
+console.log(additon(10,20));
+console.log(subtraction(10,20));
+console.log(multiplication(10,20));
+console.log(division(20,10));
+
---
+++ Modules/operations.js
@@ -0,0 +1,32 @@
+// !====== Default Export ======
+let sname ="Chirag";
+// export default sname;
+
+let obj = {
+    sname:"Chirag Mehra",
+    skills:["basic","js","react js"]
+};
+// export default obj;
+function additon(a,b)
+{
+    return a + b;
+
+}
+function subtraction(a,b)
+{
+    return a - b;
+
+}
+function multiplication(a,b)
+{
+    return a * b;
+
+}
+function division(a,b)
+{
+    return a/b;
+}
+
+
+
+export {additon,subtraction,multiplication,division} ;
